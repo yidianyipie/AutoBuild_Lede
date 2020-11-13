3cb9f945b6e2a00e4b5950c4a7194e154e7cf855
VIPABC\robingu <guxing@anio.vip>
2020-11-13 14:28:15 +0800

    chage name

--- updatefeeds.sh
+++ updatefeeds.sh
@@ -16,7 +16,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # Add Theme
-sed -i '$a src-git argon-mc https://github.com/sypopo/luci-theme-argon-mc.git' feeds.conf.default
+sed -i '$a src-git argonmc https://github.com/sypopo/luci-theme-argon-mc.git' feeds.conf.default
 sed -i '$a src-git butterfly https://github.com/Leo-Jo-My/luci-theme-Butterfly.git' feeds.conf.default
 sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy.git' feeds.conf.default
 
