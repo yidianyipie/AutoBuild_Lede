172b83824d271cee28d05f6b32df169cef3b5951
PAICDOM\GUXING210 <guxing@anio.vip>
2021-06-30 12:32:20 +0800

    修改内核模块

--- common/updateimage.sh
+++ common/updateimage.sh
@@ -28,15 +28,16 @@ echo 'CONFIG_PACKAGE_luci-theme-argon-mc=y' >> .config
 sudo mkdir /lib/firmware/i915
 sudo curl -L https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/i915/kbl_dmc_ver1_04.bin -o /lib/firmware/i915/kbl_dmc_ver1_04.bin
 
-# 添加 'i915/kbl_dmc_ver1_04.bin'
-#CONFIG_FIRMWARE_IN_KERNEL=y
-#echo 'CONFIG_FIRMWARE_IN_KERNEL=y' >> target/linux/x86/config-5.4
-#echo 'CONFIG_EXTRA_FIRMWARE="i915/kbl_dmc_ver1_04.bin"' >> target/linux/x86/config-5.4
-#echo 'CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"' >> target/linux/x86/config-5.4
+# 添加 'i915/kbl_dmc_ver1_04.bin' 到5.4的内核
+# CONFIG_FIRMWARE_IN_KERNEL=y
+echo 'CONFIG_FIRMWARE_IN_KERNEL=y' >> target/linux/x86/config-5.4
+echo 'CONFIG_EXTRA_FIRMWARE="i915/kbl_dmc_ver1_04.bin"' >> target/linux/x86/config-5.4
+echo 'CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"' >> target/linux/x86/config-5.4
 
 # 执行kernel命令
 # 查看
 ls target/linux/x86/
+# 添加 'i915/kbl_dmc_ver1_04.bin' 到5.10的内核
 echo 'CONFIG_FIRMWARE_IN_KERNEL=y' >> target/linux/x86/config-5.10
 echo 'CONFIG_EXTRA_FIRMWARE="i915/kbl_dmc_ver1_04.bin"' >> target/linux/x86/config-5.10
 echo 'CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"' >> target/linux/x86/config-5.10
