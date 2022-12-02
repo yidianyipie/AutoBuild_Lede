b8fea6e9d075cb5c5b5241294f25eb7453054952
记忆里熟悉的味道 <guxing@anio.vip>
2022-12-02 21:42:43 +0800

    Update updatefeeds.sh

--- common/updatefeeds.sh
+++ common/updatefeeds.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -24,5 +24,6 @@ sed -i '$a src-git argonmc https://github.com/sypopo/luci-theme-argon-mc.git' fe
 sed -i '$a src-git butterfly https://github.com/Leo-Jo-My/luci-theme-Butterfly.git' feeds.conf.default
 sed -i '$a src-git rosy https://github.com/rosywrt/luci-theme-rosy.git' feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 
 cat feeds.conf.default
